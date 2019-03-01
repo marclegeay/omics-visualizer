81c60d4911571e044012a182b63644e12b7fb0a8
Marc Legeay <marc.legeay@cpr.ku.dk>
2019-03-01 14:23:56 +0100

    Change the color of the disconnect button.

--- src/main/java/dk/ku/cpr/OmicsVisualizer/internal/ui/OVConnectPanel.java
+++ src/main/java/dk/ku/cpr/OmicsVisualizer/internal/ui/OVConnectPanel.java
@@ -1,6 +1,7 @@
 package dk.ku.cpr.OmicsVisualizer.internal.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -66,6 +67,7 @@ public class OVConnectPanel extends JPanel implements ActionListener {
 		this.updateButton = new JButton("Update");
 		this.updateButton.addActionListener(this);
 		this.disconnectButton = new JButton("Disconnect");
+		this.disconnectButton.setForeground(Color.RED);
 		this.disconnectButton.addActionListener(this);
 		
 		buttonPanel.add(this.updateButton);
